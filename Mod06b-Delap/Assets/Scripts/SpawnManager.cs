10450b010228f97b22f0fd2d5fddf7faa3137df0
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-03-21 13:45:57 -0500

    spawn manager started but not finished

---
+++ Mod06b-Delap/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

7339b2c950e58302f3cf0a113d9e2da25ba3947d
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-03-23 13:15:17 -0500

    make the animals move faster as time goes on

--- Mod06b-Delap/Assets/Scripts/MoveForward.cs
+++ Mod06b-Delap/Assets/Scripts/MoveForward.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 30.0f;
+    public bool dynamicSpeed = true;
 
     void Update()
     {
+        // increase speed as the game goes on so difficulty is harder
+        if (dynamicSpeed) {
+            speed += Time.realtimeSinceStartup / 10000;
+        }
         transform.Translate(Vector3.forward * Time.deltaTime * speed);        
     }
 }

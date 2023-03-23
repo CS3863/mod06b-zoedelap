7a4f7442ab5d55e0f36181144e0fa52e31c0b332
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-03-23 13:22:04 -0500

    I changed my mind, I didn't like them moving faster.

--- Mod06b-Delap/Assets/Scripts/MoveForward.cs
+++ Mod06b-Delap/Assets/Scripts/MoveForward.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 30.0f;
-    public bool dynamicSpeed = true;
 
     void Update()
     {
-        // increase speed as the game goes on so difficulty is harder
-        if (dynamicSpeed) {
-            speed += Time.realtimeSinceStartup / 10000;
-        }
         transform.Translate(Vector3.forward * Time.deltaTime * speed);        
     }
 }

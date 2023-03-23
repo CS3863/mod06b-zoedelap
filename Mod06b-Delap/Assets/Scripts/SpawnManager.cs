f59a017b49658143fc48af62beb2a49dc3e256f0
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-03-23 12:35:19 -0500

    new animals spawn

--- Mod06b-Delap/Assets/Scripts/SpawnManager.cs
+++ Mod06b-Delap/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S)) {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), 
+                animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
